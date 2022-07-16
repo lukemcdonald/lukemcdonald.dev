b23568f8bf25797f769f762ad4f9593a7ff4a635
Luke McDonald <thelukemcdonald@gmail.com>
2022-07-16 15:54:22 -0500

    initial commit

---
+++ app/routes/index.tsx
@@ -0,0 +1,8 @@
+export default function Index() {
+  return (
+    <main className="relative min-h-screen bg-white sm:flex sm:flex-col sm:items-center sm:justify-center">
+      <h1 className="text-2xl font-bold">Luke McDonald</h1>
+      <p className="text-gray-700">Software Engineer</p>
+    </main>
+  );
+}
